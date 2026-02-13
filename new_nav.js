0e292f015c963c5c96f87ff99504bbd3de9ac43f
Neil Morrison <neil.c.morrison@gmail.com>
2026-02-13 16:11:19 -0700

    Update version to bb84a52 and staging loader commit hash

--- new_nav.js
+++ new_nav.js
@@ -20,7 +20,7 @@ function initNavigationScript() {
     window.navScriptLoaded = true;
 
     // Version identifier - check in console: window.navVersion
-    window.navVersion = '2026-02-13-bbeaad5';
+    window.navVersion = '2026-02-13-bb84a52';
     if (console && console.log) {
         console.log('%cNew Nav Script Loaded', 'color: #016A1B; font-weight: bold; font-size: 12px;', 'Version:', window.navVersion);
     }
