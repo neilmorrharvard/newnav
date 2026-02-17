8206c4e9fb2125fe9473af8cc1f890dd30149790
Neil Morrison <neil.c.morrison@gmail.com>
2026-02-17 14:40:36 -0700

    Fix version hash to dd37bc3

--- new_nav.js
+++ new_nav.js
@@ -20,7 +20,7 @@ function initNavigationScript() {
     window.navScriptLoaded = true;
 
     // Version identifier - check in console: window.navVersion
-    window.navVersion = '2026-02-13-3c4d5e6';
+    window.navVersion = '2026-02-13-dd37bc3';
     if (console && console.log) {
         console.log('%cNew Nav Script Loaded', 'color: #016A1B; font-weight: bold; font-size: 12px;', 'Version:', window.navVersion);
     }
