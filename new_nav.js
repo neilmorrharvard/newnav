2b70aa1ff7f3bfd7916e890eecb6aac4778f4033
Neil Morrison <neil.c.morrison@gmail.com>
2026-02-17 14:53:51 -0700

    Update version to 6a7b8c9

--- new_nav.js
+++ new_nav.js
@@ -20,7 +20,7 @@ function initNavigationScript() {
     window.navScriptLoaded = true;
 
     // Version identifier - check in console: window.navVersion
-    window.navVersion = '2026-02-13-34608f0';
+    window.navVersion = '2026-02-13-6a7b8c9';
     if (console && console.log) {
         console.log('%cNew Nav Script Loaded', 'color: #016A1B; font-weight: bold; font-size: 12px;', 'Version:', window.navVersion);
     }
